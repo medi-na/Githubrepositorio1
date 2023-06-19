663bdd77155eab47c31ffe232342ae45a1275e6e
Mariela <medinamariela090122@gmail.com>
2023-06-19 10:10:46 -0600

    ordenando un arreglo en pararelo

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ for (var i = 0; i < Poblacion.Length; i++)
     if (Poblacion[i] == minDep)
         minDepkey = Departamento[i];
 }
-
+//ordenando con diccionario y Ordeby de LINQ
 var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 Departamento = ordenado.Keys.ToArray();
 Poblacion = ordenado.Values.ToArray();
