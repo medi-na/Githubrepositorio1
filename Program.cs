d0a69460762914c871be998697d049ad0118252b
Mariela <medinamariela090122@gmail.com>
2023-06-19 11:18:06 -0600

    Actualizando salida

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@ for (var i = 0; i < Poblacion.Length; i++)
     Console.WriteLine($"(Departamento [i],20) ==> {Poblacion[i],10:N0}");
 //Suma de toda la población y nombre de mayor y menor
 Console.WriteLine($"Población General:{Poblacion.Sum():N0}");
-Console.WriteLine($"Departamento con mayor Población:(maxDepkey)");
-Console.WriteLine($"Departamento con menor Población:(minDepkey)");
+Console.WriteLine($"Departamento con mayor Población:{maxDepkey}");
+Console.WriteLine($"Departamento con menor Población:{minDepkey}");
 
 
 
